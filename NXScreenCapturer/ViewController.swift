f9b97ea68c6f68efd94aa262e7eb04b1fc0170e7
yxibng <yaoxiaobing@axzt.net>
2023-04-19 19:18:01 +0800

    Initial Commit

---
+++ NXScreenCapturer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NXScreenCapturer
+//
+//  Created by yxibng on 2023/4/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
